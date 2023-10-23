0219b3f4e45a826b8ddd0769672510da1b272438
Christian Rodriguez <christian796rodriguez@gmail.com>
2023-10-23 08:36:08 -0400

    Initial Commit

---
+++ Instinct/InstinctApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstinctApp.swift
+//  Instinct
+//
+//  Created by Christian Rodriguez on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct InstinctApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
